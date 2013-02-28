b7465fd876ee4a905eda16a61cac17b513a6a9e5
Stefan Fodor <xtephan@gmail.com>
2013-02-28 23:51:32 +0100

    Kinect framework

--- Gateway/MainWindow.xaml.cs
+++ Gateway/MainWindow.xaml.cs
@@ -14,6 +14,15 @@ using System.Windows.Shapes;
 using System.Diagnostics;
 using Microsoft.Maps.MapControl.WPF;
 
+/*
+using Iisu;
+using Iisu.Data;
+*/
+
+using Microsoft.Kinect;
+using Coding4Fun.Kinect.Wpf;
+using System.IO;
+
 
 namespace WpfApplication1
 {
@@ -22,9 +31,72 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
+        /*
+        // iisu handle
+        private IHandle iisuHandle;
+
+        // iisu device
+        private IDevice device;
+
+        // validates iisu DataHandles
+        private bool m_valid = false;
+
+
+        // ensures we get a new frame
+        private int m_lastFrameID = -1;
+
+        // variables we're concerned with
+        private IDataHandle<float> zoomStage;
+        private IDataHandle<bool> hand1_closed;
+        private IDataHandle<bool> hand2_closed;
+
+        private IDataHandle<float> h1x;
+        private IDataHandle<float> h1y;
+
+        private IDataHandle<float> h2x;
+        private IDataHandle<float> h2y;
+        */
+        private KinectSensor Sensor;
+
+        /// <summary>
+        /// There is currently no connected sensor.
+        /// </summary>
+        private bool isDisconnectedField = true;
+
+        /// <summary>
+        /// Any message associated with a failure to connect.
+        /// </summary>
+        private string disconnectedReasonField;
+
+        /// <summary>
+        /// Array to receive skeletons from sensor, resize when needed.
+        /// </summary>
+        private Microsoft.Kinect.Skeleton[] skeletons = new Microsoft.Kinect.Skeleton[0];
+
+        /// <summary>
+        /// Time until skeleton ceases to be highlighted.
+        /// </summary>
+        private DateTime highlightTime = DateTime.MinValue;
+
+        /// <summary>
+        /// The ID of the skeleton to highlight.
+        /// </summary>
+        private int highlightId = -1;
+
+        /// <summary>
+        /// The ID if the skeleton to be tracked.
+        /// </summary>
+        private int nearestId = -1;
+
+        /// <summary>
+        /// The index of the current image.
+        /// </summary>
+        private int indexField = 1;
+
         public MainWindow()
         {
             InitializeComponent();
+            myMap.Mode = new RoadMode();
         }
 
         private void restaurant_Click(object sender, RoutedEventArgs e)  //click version
@@ -125,6 +197,149 @@ namespace WpfApplication1
             Map myMap = (Map)sender;
             //Gets the bounded rectangle for the current frame
             LocationRect bounds = myMap.BoundingRectangle;
-        }    
+        }
+
+
+
+
+
+        private void OnSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            // Get the frame.
+            using (var frame = e.OpenSkeletonFrame())
+            {
+                // Ensure we have a frame.
+                if (frame != null)
+                {
+                    // Resize the skeletons array if a new size (normally only on first call).
+                    if (this.skeletons.Length != frame.SkeletonArrayLength)
+                    {
+                        this.skeletons = new Microsoft.Kinect.Skeleton[frame.SkeletonArrayLength];
+                    }
+
+                    // Get the skeletons.
+                    frame.CopySkeletonDataTo(this.skeletons);
+
+                    // Assume no nearest skeleton and that the nearest skeleton is a long way away.
+                    var newNearestId = -1;
+                    var nearestDistance2 = double.MaxValue;
+
+                    bool __skeletonPresent = false;
+
+                    // Look through the skeletons.
+                    foreach (var skeleton in this.skeletons)
+                    {
+                        // Only consider tracked skeletons.
+                        if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
+                        {
+                            __skeletonPresent = true;
+                            // Find the distance squared.
+                            var distance2 = (skeleton.Position.X * skeleton.Position.X) +
+                                (skeleton.Position.Y * skeleton.Position.Y) +
+                                (skeleton.Position.Z * skeleton.Position.Z);
+
+                            // Is the new distance squared closer than the nearest so far?
+                            if (distance2 < nearestDistance2)
+                            {
+                                // Use the new values.
+                                newNearestId = skeleton.TrackingId;
+                                nearestDistance2 = distance2;
+                            }
+                        }
+                    }
+
+
+                    //Draw hands
+                    foreach (var skeleton in this.skeletons)
+                    {
+                        // Only consider tracked skeletons.
+                        if (skeleton.TrackingState == SkeletonTrackingState.Tracked &&
+                            skeleton.TrackingId == this.nearestId)
+                        {
+                            var scaledJointLeft = skeleton.Joints[JointType.HandLeft].ScaleTo(900, 540, .8f, .8f);
+                            var scaledJointRight = skeleton.Joints[JointType.HandRight].ScaleTo(900, 540, .8f, .8f);
+
+
+                            //SetEllipsePosition(leftEllipse, scaledJointLeft);
+                            //SetEllipsePosition(rightEllipse, scaledJointRight);
+                        }
+                    }
+
+                }
+            }
+        }
+
+
+        private void SetEllipsePosition(FrameworkElement ellipse, Joint hoverJoint)
+        {
+            Canvas.SetLeft(ellipse, hoverJoint.Position.X);
+            Canvas.SetTop(ellipse, hoverJoint.Position.Y);
+        }
+
+
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            this.InitializeNui();
+        }
+
+        private void InitializeNui()
+        {
+            this.UninitializeNui();
+
+            this.isDisconnectedField = false;
+            var index = 0;
+
+            
+                try
+                {
+                    this.Sensor = KinectSensor.KinectSensors[0];
+                    this.Sensor.Start();
+
+                }
+                catch (IOException ex)
+                {
+                    this.Sensor = null;
+
+                    this.isDisconnectedField = true;
+
+                }
+                catch (InvalidOperationException ex)
+                {
+                    this.Sensor = null;
+                }
+
+
+
+            if (this.Sensor != null)
+            {
+                //set to near mode
+                this.Sensor.SkeletonStream.EnableTrackingInNearRange = true;
+                this.Sensor.DepthStream.Range = DepthRange.Near; // Depth in near range enabled
+                this.Sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+                this.Sensor.SkeletonStream.Enable();
+
+                this.Sensor.SkeletonFrameReady += this.OnSkeletonFrameReady;
+
+            }
+
+
+        }
+
+        /// <summary>
+        /// Handle removal of Kinect sensor.
+        /// </summary>
+        private void UninitializeNui()
+        {
+            if (this.Sensor != null)
+            {
+                this.Sensor.SkeletonFrameReady -= this.OnSkeletonFrameReady;
+
+                this.Sensor.Stop();
+
+                this.Sensor = null;
+            }
+        }
+
     }
 }
